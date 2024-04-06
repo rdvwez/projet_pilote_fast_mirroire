dda53527503189118561c6f1aa29447a0214ac60
vwez <desirv63@gmail.com>
2024-04-06 15:51:47 +0200

    update app/main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
 
+from app.models.post import UserPost, UserPostIn
+
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {'message':'hello wordl !'}
+
+post_table = {}
+
+
+@app.post("/", response_model=UserPost)
+async def create_post(post: UserPostIn):
+    data = post.dict()
+    last_record_id = len(data)
+
+    new_post = {**data, "id": last_record_id}
+    post_table[last_record_id] = new_post
+    return new_post
+
+
+@app.post("/post", response_model=list[UserPost])
+async def get_all_posts():
+    return list(post_table.values())
+    return list(post_table.values())
